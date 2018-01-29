cb522618c905f99e8d5f0addec202b26a4bccd21
Tavis Reddick <developer@sleepingdog.org.uk>
2018-01-29 01:18:45 +0000

    SELECT from file SQL script
    Added utf-8 codepage (required upgrade to SQL Server 2016 Express
    database engine) and converted '\N' values to proper database NULLs.

--- SELECT-from-files.sql
+++ SELECT-from-files.sql
@@ -1,15 +1,26 @@
 USE IMDb;
 
-SELECT *
+SELECT tconst, averageRating, numVotes
 FROM OPENROWSET (
-	BULK '\\WDMYCLOUDMIRROR\SleepingDog\Projects\Data\IMdb-sql-queries\title.ratings.tsv',   
-	FORMATFILE = '\\WDMYCLOUDMIRROR\SleepingDog\Projects\Data\IMdb-sql-queries\title-ratings.xml' 
+	BULK 'I:\title.ratings.tsv',   
+	FORMATFILE = 'I:\title-ratings.xml' 
 ) AS test1;
 GO
-SELECT *
+SELECT tconst, principalCast
 FROM OPENROWSET (
-	BULK '\\WDMYCLOUDMIRROR\SleepingDog\Projects\Data\IMdb-sql-queries\title.principals.tsv',   
-	FORMATFILE = '\\WDMYCLOUDMIRROR\SleepingDog\Projects\Data\IMdb-sql-queries\title-principals.xml' 
+	BULK 'I:\title.principals.tsv',   
+	FORMATFILE = 'I:\title-principals.xml' 
 ) AS test2;
 GO
-
+SELECT titleId, ordering, title
+	, CAST((CASE WHEN region = '\N' THEN NULL ELSE region END) AS CHAR(5)) AS region
+	, CAST((CASE WHEN language = '\N' THEN NULL ELSE language END) AS CHAR(10)) AS language
+	, CAST((CASE WHEN types = '\N' THEN NULL ELSE types END) AS VARCHAR) AS types
+	, CAST((CASE WHEN attributes = '\N' THEN NULL ELSE attributes END) AS VARCHAR) AS attributes
+	, CAST((CASE WHEN isOriginalTitle = '\N' THEN NULL ELSE isOriginalTitle END) AS BIT) AS isOriginalTitle
+FROM OPENROWSET (
+	BULK 'I:\title.akas.tsv',   
+	FORMATFILE = 'I:\title-akas.xml',
+	CODEPAGE = '65001'
+) AS test2;
+GO
