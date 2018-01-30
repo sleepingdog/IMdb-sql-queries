3097a7cbdd71fb47ef6e1116c2083111a7ed036e
Tavis Reddick <developer@sleepingdog.org.uk>
2018-01-30 01:41:33 +0000

    SELECT INTO TABLEs SQL
    Added dropping and creating database, and first table creation and
    population from file (successful test).

--- 1-SELECT-INTO-TABLEs.sql
+++ 1-SELECT-INTO-TABLEs.sql
@@ -4,6 +4,16 @@
 To simply file paths, the drive letter I: has been mapped to
 the directory containing all the relevant files. Do this first.
 */
+-- DROP existing IMDb database
+USE [master]
+GO
+/*
+ALTER DATABASE IMDb
+SET OFFLINE;
+GO
+*/
+DROP DATABASE IF EXISTS IMDb;
+GO
 -- CREATE IMDb database
 CREATE DATABASE IMDb;
 GO
@@ -22,6 +32,20 @@ GO
 SELECT * INTO   
 GO
 */
+SELECT titleId, ordering, title
+	, CAST((CASE WHEN region = '\N' THEN NULL ELSE region END) AS CHAR(5)) AS region
+	, CAST((CASE WHEN language = '\N' THEN NULL ELSE language END) AS CHAR(10)) AS language
+	, CAST((CASE WHEN types = '\N' THEN NULL ELSE types END) AS VARCHAR) AS types
+	, CAST((CASE WHEN attributes = '\N' THEN NULL ELSE attributes END) AS VARCHAR) AS attributes
+	, CAST((CASE WHEN isOriginalTitle = '\N' THEN NULL ELSE isOriginalTitle END) AS BIT) AS isOriginalTitle
+	INTO movie.titleakas
+FROM OPENROWSET (
+	BULK 'I:\title.akas.tsv',   
+	FORMATFILE = 'I:\title-akas.xml',
+	FIRSTROW = 2,
+	CODEPAGE = '65001' -- utf-8, requires SS2016+
+) AS titleakas; -- approx 3559746 rows
+GO
 -- Restore logging
 ALTER DATABASE IMDb SET RECOVERY FULL;  
 GO  
