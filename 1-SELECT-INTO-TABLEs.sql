aa3abc93c4494411677a50a7a767a68b401be13e
Tavis Reddick <developer@sleepingdog.org.uk>
2018-01-30 03:26:27 +0000

    SELECT INTO TABLEs SQL
    Finished adding table create code.

--- 1-SELECT-INTO-TABLEs.sql
+++ 1-SELECT-INTO-TABLEs.sql
@@ -46,6 +46,72 @@ FROM OPENROWSET (
 	CODEPAGE = '65001' -- utf-8, requires SS2016+
 ) AS titleakas; -- approx 3559746 rows
 GO
+SELECT tconst, titleType, primaryTitle, originalTitle,
+	isAdult
+	, CAST((CASE WHEN startYear = '\N' THEN NULL ELSE startYear END) AS SMALLINT) AS startYear
+	, CAST((CASE WHEN endYear = '\N' THEN NULL ELSE endYear END) AS SMALLINT) AS endYear
+	, CAST((CASE WHEN runtimeMinutes = '\N' THEN NULL ELSE runtimeMinutes END) AS INT) AS runtimeMinutes
+	, CAST((CASE WHEN genres = '\N' THEN NULL ELSE genres END) AS NVARCHAR(50)) AS genres
+	INTO movie.titlebasics
+FROM OPENROWSET (
+	BULK 'I:\title.basics.tsv',
+	FORMATFILE = 'I:\title-basics.xml',
+	FIRSTROW = 2,
+	CODEPAGE = '65001' -- utf-8, requires SS2016+
+) AS titlebasics; -- approx 4760383 rows
+GO
+SELECT tconst
+	, (CASE WHEN directors = '\N' THEN NULL ELSE directors END) AS directors
+	, (CASE WHEN writers = '\N' THEN NULL ELSE writers END) AS writers
+	INTO movie.titlecrew
+FROM OPENROWSET (
+	BULK 'I:\title.crew.tsv',
+	FORMATFILE = 'I:\title-crew.xml',
+	FIRSTROW = 2,
+	CODEPAGE = '65001' -- utf-8, requires SS2016+
+) AS titlecrew; -- approx 4219 and 5659 characters respectively
+GO
+SELECT tconst, parentTconst
+	, CAST((CASE WHEN seasonNumber = '\N' THEN NULL ELSE seasonNumber END) AS SMALLINT) AS seasonNumber
+	, CAST((CASE WHEN episodeNumber = '\N' THEN NULL ELSE episodeNumber END) AS INT) AS episodeNumber
+	INTO movie.titleepisode
+FROM OPENROWSET (
+	BULK 'I:\title.episode.tsv',
+	FORMATFILE = 'I:\title-episode.xml',
+	FIRSTROW = 2,
+	CODEPAGE = '65001' -- utf-8, requires SS2016+
+) AS titleepisode;  -- approx 3167900 rows
+GO
+SELECT tconst, principalCast
+	INTO movie.titleprincipals
+FROM OPENROWSET (
+	BULK 'I:\title.principals.tsv',   
+	FORMATFILE = 'I:\title-principals.xml',
+	FIRSTROW = 2,
+	CODEPAGE = '65001' -- utf-8, requires SS2016+
+) AS titleprincipals; -- approx 4234452 rows
+GO
+SELECT tconst, averageRating, numVotes
+	INTO movie.titleratings
+FROM OPENROWSET (
+	BULK 'I:\title.ratings.tsv',   
+	FORMATFILE = 'I:\title-ratings.xml',
+	FIRSTROW = 2,
+	CODEPAGE = '65001' -- utf-8, requires SS2016+
+) AS titleratings; -- approx 798666 rows
+GO
+SELECT nconst, primaryName
+	, CAST((CASE WHEN birthYear = '\N' THEN NULL ELSE birthYear END) AS SMALLINT) AS birthYear
+	, CAST((CASE WHEN deathYear = '\N' THEN NULL ELSE deathYear END) AS SMALLINT) AS deathYear
+	, primaryProfession, knownForTitles
+	INTO movie.namebasics
+FROM OPENROWSET (
+	BULK 'I:\name.basics.tsv',
+	FORMATFILE = 'I:\name-basics.xml',
+	FIRSTROW = 2,
+	CODEPAGE = '65001' -- utf-8, requires SS2016+
+) AS namebasics; -- approx 8378045 rows
+GO
 -- Restore logging
 ALTER DATABASE IMDb SET RECOVERY FULL;  
 GO  
