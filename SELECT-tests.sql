6f7c01c81ea6172f2565163de8d63c2b5f03329c
Tavis Reddick <developer@sleepingdog.org.uk>
2018-01-31 20:33:16 +0000

    Format file updated to fix decimal import problem
    The XML format file needed two new attributes (SCALE and PRECISION) to
    fix NUMERIC column import.

--- SELECT-tests.sql
+++ SELECT-tests.sql
@@ -12,11 +12,10 @@ FROM movie.titleratings
 GROUP BY averageRating
 ORDER BY averageRating ASC;
 
--- ### problem: rounding of averageRating ?
-
 SELECT tb.primaryTitle
 	, e2.primaryTitle AS episodeTitle, e2.startYear, e2.runtimeMinutes
 	, e1.seasonNumber, e1.episodeNumber
+	, tp.principalCast
 FROM movie.titlebasics AS tb
 	INNER JOIN
 		movie.titleepisode AS e1
@@ -24,6 +23,9 @@ FROM movie.titlebasics AS tb
 	INNER JOIN
 		movie.titlebasics AS e2
 			ON e1.tconst = e2.tconst
+	LEFT OUTER JOIN
+		movie.titleprincipals AS tp
+			ON tp.tconst = e1.tconst
 WHERE tb.primaryTitle = 'Doctor Who' AND tb.titleType = 'tvSeries'
 ORDER BY e2.startYear ASC, e1.seasonNumber ASC, e1.episodeNumber ASC;
 
